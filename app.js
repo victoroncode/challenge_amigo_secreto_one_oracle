954a9ec89de963ef919ec6314b7cf77b60f5981c
Paulo Victor de Oliveira Silva <p2.paulovictor.p2@gmail.com>
2025-03-18 23:08:07 -0300

    Adicionar conexão entre o "Enter" no input e ativação no botão

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ let amigos = [];
 function adicionarAmigo() {
   amigo = document.getElementById("amigo");
 
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
   if (amigo.value == "") {
     alert("Por favor, insira um nome válido.");
   } else {
-    amigos.push(amigo.value);
+    amigos.push(amigo.value.trim());
     amigo.value = "";
     atualizarListaAmigos();
   }
@@ -24,12 +27,25 @@ function atualizarListaAmigos() {
 }
 
 function sortearAmigo() {
-  let quantidadeDeAmigos = amigos.length;
-  let amigoSorteado = amigos[Math.floor(Math.random() * quantidadeDeAmigos)];
-  let resultado = document.getElementById("resultado");
-  resultado.innerHTML = `O amigo sorteado é: ${amigoSorteado}!`;
+  if (amigos.length > 0) {
+    let quantidadeDeAmigos = amigos.length;
+    let amigoSorteado = amigos[Math.floor(Math.random() * quantidadeDeAmigos)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado é: ${amigoSorteado}!`;
 
-  let listaAmigos = document.getElementById("listaAmigos");
-  listaAmigos.innerHTML = "";
-  amigos = [];
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    amigos = [];
+  } else {
+    alert("Por favor, adicione os nomes para realizer o sorteio.");
+  }
 }
+
+let input = document.getElementById("amigo");
+let button = document.getElementById("button");
+
+input.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    button.click();
+  }
+});
